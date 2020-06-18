9f692a28213d31169a87e950404b79319c58f50e
Omer Mokady <omer.mokady@live.biu.ac.il>
2020-06-18 20:28:59 +0300

    edit text for typing URL

--- app/src/main/java/com/combo/flightmobileapp/MainActivity.kt
+++ app/src/main/java/com/combo/flightmobileapp/MainActivity.kt
@@ -2,11 +2,20 @@ package com.combo.flightmobileapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
+import android.widget.TextView
+
 
 class MainActivity : AppCompatActivity() {
+
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         //test for mokady git
     }
+
+
 }
